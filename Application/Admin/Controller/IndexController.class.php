ca054c8011386020a88b7e661033bae0aa88602a
maming <494156361@qq.com>
2016-11-11 10:21:13 +0800

    day1:管理员模块，前台首页

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace Admin\Controller;
+use \Think\Controller;
+
+class IndexController extends AdminController 
+{
+    public function index(){
+        if ($_SESSION['state'] == 0) {
+            $identity = "超级管理员";
+        } else {
+            $identity = "普通管理员";
+        }
+        $name = $_SESSION['name'];
+        $this->assign('name',$name);
+        $this->assign('identity',$identity);
+   	    $this->display();
+    }
+
+    public function welcome()
+    {
+   	    $this->display();
+    }
+
+    public function yzm()
+    {
+        $Verify = new \Think\Verify();
+        $Verify->fontSize = 16;
+        $Verify->length = 4;
+        $Verify->codeSet = '0123456789';
+        $Verify->imageW = 100;
+        $Verify->imageH = 40;
+        $Verify->entry();
+    }
+}
